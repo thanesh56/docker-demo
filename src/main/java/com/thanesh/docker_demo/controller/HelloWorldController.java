168ee1729043cf394fa09a8bd373ce009027559f
thanesh56 <thaneshwarsahu58@gmail.com>
2025-08-17 13:42:36 +0530

    Added new endpoint

--- src/main/java/com/thanesh/docker_demo/controller/HelloWorldController.java
+++ src/main/java/com/thanesh/docker_demo/controller/HelloWorldController.java
@@ -10,4 +10,12 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello, World!, Java, Python, Node.js, Go, C++, Rust, C#, PHP, Ruby, Swift, Kotlin, Dart, Scala, Elixir, Haskell, Lua, Perl, Groovy, TypeScript, JavaScript, HTML, CSS, SQL, Bash/Shell scripting";
     }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hi, this is for testing working of github and jenkins integration with docker and kubernetes. " +
+                "\nThis is a simple application to demonstrate the CI/CD pipeline using Jenkins, Docker, and Kubernetes. " +
+                "\nIt is built using Spring Boot and serves a simple message when accessed via HTTP GET request. " +
+                "\nThe application can be containerized using Docker and deployed on a Kubernetes cluster for scalability and reliability.";
+    }
 }
