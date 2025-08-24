a89d3f0eeb3e1e773bc2aa47879101fd4c52a929
thanesh56 <thaneshwarsahu58@gmail.com>
2025-08-24 20:00:28 +0530

    Added docker configuration

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # docker-demo
+
+### For building image
+```
+   docker build -t spring-docker-demo:1.0.1 .
+```
+
+### To access local container to minikube 
+```
+    eval $(minikube docker-env)
+```
+
+### To remove access local container to minikube
+```
+    eval $(minikube docker-env --unset)
+```
+
+### For loading image in minikube 
+```
+    minikube image load spring-docker-demo:1.0.1
+```
+
+### Create namespace 
+```
+    kubectl create namespace spring-docker-demo
+```
+
+### Command to run image as deployment  
+```
+    kubectl apply -f springboot-k8s-deployment.yaml -n spring-docker-demo
+```
+
+### Command to get all deployment in namespace 
+```
+    kubectl get all -n spring-docker-demo
+```
+
+### Tail the logs 
+```
+    kubectl logs <POD_NAME> -n spring-docker-demo -f 
+```
+
+### Port-forwarding is required in case you use docker as driver for minikube
+```
+    minikube service spring-docker-service -n spring-docker-demo
+    #or
+    # kubeclt port-forward svc/webapp-service <CUSTOME_HOST>:<K8S/SERVICE/POD>
+```
--- src/main/java/com/thanesh/docker_demo/controller/HelloWorldController.java
+++ src/main/java/com/thanesh/docker_demo/controller/HelloWorldController.java
@@ -1,18 +1,24 @@
 package com.thanesh.docker_demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @GetMapping("/hello")
     public String helloWorld() {
+        logger.info("Hello endpoint was called");
         return "Hello, World!, Java, Python, Node.js, Go, C++, Rust, C#, PHP, Ruby, Swift, Kotlin, Dart, Scala, Elixir, Haskell, Lua, Perl, Groovy, TypeScript, JavaScript, HTML, CSS, SQL, Bash/Shell scripting";
     }
 
     @GetMapping("/hi")
     public String hi() {
+        logger.info("hi endpoint was called");
         return "Hi, this is for testing working of github and jenkins integration with docker and kubernetes. " +
                 "\nThis is a simple application to demonstrate the CI/CD pipeline using Jenkins, Docker, and Kubernetes. " +
                 "\nIt is built using Spring Boot and serves a simple message when accessed via HTTP GET request. " +
